6657dffa9324a9994e8fa90e28dbac923f59f61d
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2022-05-15 18:09:05 -0400

    Working on charts

--- BudgetBlazor/Pages/Index.razor.cs
+++ BudgetBlazor/Pages/Index.razor.cs
@@ -1,9 +1,26 @@
 ﻿using Microsoft.AspNetCore.Components;
-using MudBlazor;
+using Plotly.Blazor;
+using Plotly.Blazor.Traces;
+using Plotly.Blazor.Traces.ScatterLib;
 
 namespace BudgetBlazor.Pages
 {
     public class IndexBase : ComponentBase
     {
+        protected PlotlyChart chart;
+        protected Config config = new Config();
+        protected Layout layout = new Layout();
+
+        // Using of the interface IList is important for the event callback!
+        protected IList<ITrace> data = new List<ITrace>
+        {
+            new Scatter
+            {
+                Name = "ScatterTrace",
+                Mode = ModeFlag.Lines | ModeFlag.Markers,
+                X = new List<object>{1,2,3},
+                Y = new List<object>{1,2,3}
+            }
+        };
     }
 }
