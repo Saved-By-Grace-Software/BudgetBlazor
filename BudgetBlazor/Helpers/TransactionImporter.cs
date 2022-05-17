f3418fdb7afb847bff327e3e11e8cf2fd7baef9e
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2022-05-17 11:19:20 -0400

    Import square CSV file

--- BudgetBlazor/Helpers/TransactionImporter.cs
+++ BudgetBlazor/Helpers/TransactionImporter.cs
@@ -1,5 +1,8 @@
-﻿using DataAccess.Services;
+﻿using CsvHelper;
+using DataAccess.Services;
+using Microsoft.AspNetCore.Components.Forms;
 using OfxSharp;
+using System.Globalization;
 using BudgetAccount = DataAccess.Models.Account;
 using BudgetTransaction = DataAccess.Models.Transaction;
 
@@ -7,7 +10,14 @@ namespace BudgetBlazor.Helpers
 {
     public static class TransactionImporter
     {
-        public static bool ImportTransactions(string transactionsFile, BudgetAccount account, IBudgetDataService dataService)
+        /// <summary>
+        /// Imports transactions from an OFX file
+        /// </summary>
+        /// <param name="transactionsFile">String buffer containing file contents</param>
+        /// <param name="account">Account to import the transactions into</param>
+        /// <param name="dataService">Data service for adding transactions into the database</param>
+        /// <returns></returns>
+        public static bool ImportOFXTransactions(string transactionsFile, BudgetAccount account, IBudgetDataService dataService)
         {
             bool success = false;
 
@@ -52,6 +62,84 @@ namespace BudgetBlazor.Helpers
 
                     success = true;
                 }
+            }
+            catch (Exception)
+            {
+                // Failed to parse transactions, return false
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// Imports transactions from a CSV file
+        /// </summary>
+        /// <param name="transactionsFile">String buffer containing file contents</param>
+        /// <param name="account">Account to import the transactions into</param>
+        /// <param name="dataService">Data service for adding transactions into the database</param>
+        /// <returns></returns>
+        public static bool ImportCSVTransactions(string transactionsFile, BudgetAccount account, IBudgetDataService dataService)
+        {
+            bool success = false;
+            List<dynamic> records = new List<dynamic>();
+
+            try
+            {
+                // Parse the csv records into a list
+                using (var csv = new CsvReader(new StringReader(transactionsFile), CultureInfo.InvariantCulture))
+                {
+                    records = csv.GetRecords<dynamic>().ToList();
+                }
+
+                // Temp variables to store the most recent balance
+                DateTime latestBalanceDate = DateTime.MinValue;
+                decimal latestBalance = 0;
+
+                // Iterate through the records to create transactions
+                foreach (IDictionary<string, object> record in records)
+                {
+                    // Get the data from the record
+                    string transactionName = (string)record["Details"];
+
+                    if (DateTime.TryParse((string)record["Date"], out DateTime transactionDate) &&
+                        decimal.TryParse((string)record["Amount"], NumberStyles.Currency, null, out decimal transactionAmount))
+                    {
+                        // Create a new budget transaction
+                        BudgetTransaction t = new BudgetTransaction(transactionName, account.User);
+                        t.Amount = transactionAmount;
+                        t.TransactionDate = transactionDate;
+
+                        // Add the transaction to the account if it is unique
+                        if (!account.Transactions.Contains(t))
+                        {
+                            // Run automations against the transaction
+                            t = AutomationEngine.ExecuteAllAutomations(t, account.User, dataService);
+
+                            // Add the transaction to the account
+                            account.Transactions.Add(t);
+                        }
+
+                        // Check if this is the latest balance to update latest balance
+                        if (transactionDate > latestBalanceDate &&
+                            decimal.TryParse((string)record["Balance"], NumberStyles.Currency, null, out decimal balanceAmount))
+                        {
+                            latestBalanceDate = transactionDate;
+                            latestBalance = balanceAmount;
+                        }
+                    }
+                }
+
+                // Update the account balance
+                account.CurrentBalance = latestBalance;
+                account.LastUpdated = latestBalanceDate;
+
+                // Update the account in the database
+                dataService.Update(account);
+
+                // Add the account history to the database
+                dataService.UpdateAccountHistory(account, latestBalanceDate, latestBalance);
+
+                success = true;
             }
             catch (Exception)
             {
