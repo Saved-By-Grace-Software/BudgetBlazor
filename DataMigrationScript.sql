a3abe33cdfbeb4ad5d52593fa4c0a9af708e4686
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2022-05-19 10:11:55 -0400

    Transfer transactions

--- DataMigrationScript.sql
+++ DataMigrationScript.sql
@@ -9,6 +9,21 @@ SELECT @NextMonthId = IDENT_CURRENT('BudgetBlazor.dbo.BudgetMonths');
 DECLARE @NextCatId AS INT
 SELECT @NextCatId = IDENT_CURRENT('BudgetBlazor.dbo.BudgetCategory');
 
+-- Get the offset for the budget item ID
+DECLARE @NextItemId AS INT
+SELECT @NextItemId = IDENT_CURRENT('BudgetBlazor.dbo.BudgetItem');
+
+-- Create temp mapping table for account mapping
+CREATE TABLE #AccountMap
+(
+	SourceAccount INT,
+	DestAccount INT
+)
+INSERT INTO #AccountMap VALUES (1, 3)
+INSERT INTO #AccountMap VALUES (2, 2)
+INSERT INTO #AccountMap VALUES (3, 1)
+INSERT INTO #AccountMap VALUES (6, 1)
+
 -- Create temp lookup table for the category enum
 CREATE TABLE #CategoryEnum
 (
@@ -51,10 +66,23 @@ INNER JOIN [BudgetBlazor].[dbo].[BudgetMonths] on [BudgetBlazor].[dbo].[BudgetMo
 Set IDENTITY_INSERT [BudgetBlazor].[dbo].[BudgetCategory] Off
 
 -- Copy over the budget items
-INSERT INTO [BudgetBlazor].[dbo].[BudgetItem]
-SELECT [BID].[Name], [BID].[Budgeted] as [Budget], [BID].[Spent], [BID].[Remaining], ([CategoryID] + @NextCatId) as [BudgetCategoryId]
+Set IDENTITY_INSERT [BudgetBlazor].[dbo].[BudgetItem] On
+INSERT INTO [BudgetBlazor].[dbo].[BudgetItem] ([Id], [Name], [Budget], [Spent], [Remaining], [BudgetCategoryId])
+SELECT ([BudgetItemDataID] + @NextItemId) as [Id], [BID].[Name], [BID].[Budgeted] as [Budget], [BID].[Spent], [BID].[Remaining], ([CategoryID] + @NextCatId) as [BudgetCategoryId]
 FROM [Budget].[dbo].[BudgetItemDatas] as [BID]
 INNER JOIN [BudgetBlazor].[dbo].[BudgetCategory] on [BudgetBlazor].[dbo].[BudgetCategory].[Id] = ([CategoryID] + @NextCatId)
+Set IDENTITY_INSERT [BudgetBlazor].[dbo].[BudgetItem] Off
+
+-- Copy over the transactions
+INSERT INTO [BudgetBlazor].[dbo].[Transactions] ([Name], [FITransactionId], [Amount], [CheckNumber], [TransactionDate], [IsSplit], [IsPartial], [User], [BudgetId], [AccountId], [IsIncome])
+SELECT [Budget].[dbo].[Transactions].[Name], [TransactionID] as [FITransactionId], [Amount], [CheckNumber], [TransactionDate], [IsSplit], [IsPartial],
+	@DestinationUserId as [User],
+	(SELECT IIF(BudgetItemDataID < 1, NULL, ([BudgetItemDataID] + @NextItemId)) FROM [Budget].[dbo].[Transactions] as [InTran] WHERE [Budget].[dbo].[Transactions].[ID] = [InTran].[ID]) as [BudgetId],
+	(SELECT [DestAccount] FROM #AccountMap WHERE [SourceAccount] = [TransactionsAccountId]) as [AccountId],
+	0 as [IsIncome]
+FROM [Budget].[dbo].[Transactions]
+WHERE [Budget].[dbo].[Transactions].[UserID] = @SourceUserId
 
 -- Clear the temp tables
 DROP TABLE #CategoryEnum
+DROP TABLE #AccountMap
